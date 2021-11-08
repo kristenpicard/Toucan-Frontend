2fad18a7a58f8d1b89fd4501d3530bfbcbc2817a
Kristen Picard <kristenpicard93@gmail.com>
2021-11-08 11:01:07 -0600

    Added background image and text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import "./App.css";
+import background from "./toucan.jpg";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    // <div className="head-text">
+    //   <div className="head-img">
+    //     <img src={background} alt="toucan" />
+    //   </div>
+    //   <div className="text-on-image">
+    //     <h1>Toucan.</h1>
+    //   </div>
+    // </div>
+
+    <div
+      className="bg_image"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        color: "#f5f5f5",
+      }}
+    >
+      <h1 className="text-on-image">Toucan.</h1>
+    </div>
+  );
 }
 
 export default App;
